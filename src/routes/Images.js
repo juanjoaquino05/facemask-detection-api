ae054fa81654c050d3d3b65527cf589e9821f9db
Juan Henriquez <juanjoaquino05@gmail.com>
2021-04-16 17:52:51 -0400

    Adding Form Multipart handler form image post

--- src/routes/Images.js
+++ src/routes/Images.js
@@ -1,4 +1,14 @@
 const express = require('express')
+const multer = require('multer')
+
+const maxFileSizeMB = 0 * 1024 * 1024
+const acceptedImageMimeTypes = [
+    'image/jpeg', 'image/png', 'image/jpg'
+]
+
+const upload = multer({
+    limits: { fileSize: maxFileSizeMB }
+}).single('image')
 
 const images = express.Router();
 
@@ -6,8 +16,26 @@ images.use(function(req, res, next) {
     next()
 });
 
-images.post('/detect', function(resquest, response) {
-    response.json(process.env)
+images.post('/detect', (request, response) => {
+    upload(request, response, function (err) {
+        if (err instanceof multer.MulterError) {
+          // A Multer error occurred when uploading.
+          response.json({ "error" : err.message }, 500)
+        } else if (err) {
+          // An unknown error occurred when uploading.
+          response.json({ "error" : err.message }, 500)
+        }
+    
+        // Everything went fine.
+        const image = request.file
+
+        if(acceptedImageMimeTypes.indexOf(image.mimetype) < 0){
+            response.json({ "error" : "Invalid Image Type" }, 500)
+        }
+
+        response.json({ "1" : "" });
+    })
+    
 });
 
 images.get('/params', function(resquest, response) {
